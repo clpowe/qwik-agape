8682fd746697fa64f320e714a09527bc03c0cf3c
clpowe <clpowe@gmail.com>
2023-06-25 06:20:41 -0400

    updated cta

--- src/components/ContentSectionOne/index.tsx
+++ src/components/ContentSectionOne/index.tsx
@@ -1,31 +1,30 @@
 import { component$, useStylesScoped$ } from '@builder.io/qwik'
-import styles from './ContentSectionOne.module.css?inline'
+import style from './ContentSectionOne.css?inline'
 
 interface ContentSectionOneProps {
-	eyebrow: string
 	heading: string
 	body: string
 	img: string
+	ctaText: string
 }
 
 export default component$<ContentSectionOneProps>(
-	({ eyebrow, heading, body, img }) => {
-		useStylesScoped$(styles)
+	({ heading, body, img, ctaText }) => {
+		useStylesScoped$(style)
 		return (
 			<section class='section'>
-				<div class='container mx-auto px-4 holder'>
-					<p>{eyebrow}</p>
+				<div class='container mx-auto px-4 grid grid-cols-1 md:grid-cols-2'>
 					<div>
-						<h2>{heading}</h2>
-						<p>
-							{body}
-							{img}
-						</p>
+						<h2 class='text-4xl font-bold max-w-[10ch]'>{heading}</h2>
+						<p class='text-base text-gray-500 '>{body}</p>
+						<button>{ctaText}</button>
 					</div>
+
 					<img
-						src='/public/pexels-stanley-morales-3186386.jpg'
-						class='rounded-3xl'
+						src='/gradsm.jpg'
+						class='rounded-3xl h-[14.5rem] w-full object-cover md:h-[27rem]'
 						alt=''
+						height={300}
 					/>
 				</div>
 			</section>
