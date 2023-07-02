44297fe8e10499fbe9a6cde47c2f6348a55ee8da
clpowe <clpowe@gmail.com>
2023-07-01 23:33:13 -0400

    July1

--- src/components/TeacherCard/index.tsx
+++ src/components/TeacherCard/index.tsx
@@ -1,5 +1,31 @@
-import { component$ } from '@builder.io/qwik'
+import { component$, useStylesScoped$ } from '@builder.io/qwik'
+import style from './teacherCard.css?inline'
 
-export default component$(() => {
-	return <>Card</>
+type Expertise = { expertise: string }
+interface TeacherCardProps {
+	name: string
+	expertise: Expertise[]
+	image: string
+}
+
+export default component$<TeacherCardProps>(({ name, expertise, image }) => {
+	useStylesScoped$(style)
+	return (
+		<div class='card'>
+			<img src={image} alt='' class='rounded-xl' height='300' width='300' />
+			<div class='p-2'>
+				<h3 class='text-2xl uppercase font-bold mb-2'>{name}</h3>
+				<div class='space-y-1'>
+					<p class='font-bold uppercase'>Expertise</p>
+					<ul>
+						{expertise.map((e) => (
+							<li key={e.expertise} class='text-gray-6'>
+								{e.expertise}
+							</li>
+						))}
+					</ul>
+				</div>
+			</div>
+		</div>
+	)
 })
--- src/components/TeacherSection/index.tsx
+++ src/components/TeacherSection/index.tsx
@@ -1,15 +1,50 @@
-import { component$ } from '@builder.io/qwik'
+import {
+	component$,
+	useStylesScoped$,
+	useResource$,
+	Resource
+} from '@builder.io/qwik'
 import TeacherCard from '../TeacherCard'
+import style from './teacherSection.css?inline'
+
+import { getAllContent } from '@builder.io/sdk-qwik'
+
+export const apiKey = 'a77f4a06dd2947ec9095c8f325ed362e'
 
 export default component$(() => {
+	useStylesScoped$(style)
+
+	const teachersResource = useResource$(() =>
+		getAllContent({
+			model: 'teachers',
+			apiKey: apiKey
+		})
+	)
+
 	return (
 		<section>
-			<div class='myContainer'>
-				<TeacherCard />
-				<TeacherCard />
-				<TeacherCard />
-				<TeacherCard />
-				<TeacherCard />
+			<div class='myContainer grid gap-4 grid-cols-2 sm:grid-cols-4 md:grid-cols-6'>
+				<Resource
+					value={teachersResource}
+					onPending={() => <>Loading...</>}
+					onRejected={(error) => <>Error: {error.message}</>}
+					onResolved={(teachers: any) => {
+						return (
+							<>
+								{teachers?.results.map((teacher: any) => {
+									return (
+										<TeacherCard
+											key={teacher.id}
+											name={teacher.data.name}
+											expertise={teacher.data.expertiselist}
+											image={teacher.data.thumbnail}
+										/>
+									)
+								})}
+							</>
+						)
+					}}
+				/>
 			</div>
 		</section>
 	)
